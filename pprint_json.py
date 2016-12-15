0ac25635bde5b2efd9a902890767c4f795de580a
psy4ak <psy4ak@gmail.com>
2016-12-15 16:11:33 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
 
-
 def load_data(filepath):
-    pass
+    with open(filepath) as loads:
+        my_data = json.load(loads)
+        return my_data
 
 
 def pretty_print_json(data):
-    pass
+    print (json.dumps(data,ensure_ascii=False, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+
+    path_file=input("File path (by default - Enter): ")
+    
+    if path_file == "":
+        path_file= 'databars.json'
+        
+    my_data = load_data (path_file)
+    pretty_print_json (my_data)
