a979937a82e383ff2591e87bd2ea870ebe14351c
psy4ak <psy4ak@gmail.com>
2016-12-16 10:51:46 +0300

    corrected_v1
    - corrected code
    - style fix: using Python style guide checker pep8 1.7.0

--- pprint_json.py
+++ pprint_json.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_data(filepath):
     with open(filepath) as loads:
         my_data = json.load(loads)
@@ -7,15 +8,13 @@ def load_data(filepath):
 
 
 def pretty_print_json(data):
-    print (json.dumps(data,ensure_ascii=False, indent=4, sort_keys=True))
+    print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
 
-    path_file=input("File path (by default - Enter): ")
-    
-    if path_file == "":
-        path_file= 'databars.json'
-        
-    my_data = load_data (path_file)
-    pretty_print_json (my_data)
+    path_file = input("File path (by default - Enter): ")
+    if not path_file:
+        path_file = 'databars.json'
+    my_data = load_data(path_file)
+    pretty_print_json(my_data)
